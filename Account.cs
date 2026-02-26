1ccda7590d220507a811501e849a93e80f5798da
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-26 11:52:16 -0600

    Happy :)

--- Account.cs
+++ Account.cs
@@ -5,5 +5,18 @@ public class Account {
     public static int[] CurrentLedger = {10, 20, 30, 40, 50};
 
     // Add your code here
+    public int NewTaxRate = OriginalTaxRate;
+    public int[] CopyLedger = CurrentLedger;
+    
+    public int UpdateLedger(correction){
+        CurrentLedger[0] = correction;
+        return CurrentLedger;
+    }
+    
+    public int AdjustTaxRate(taxRate){
+        NewTaxRate = taxRate;
+        return NewTaxRate;
+    }
 }
+
 
